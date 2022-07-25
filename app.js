136e16155dc75fa5b1ed10cc6c4125a349dbebe6
YassineT7 <yassine.tayachi@esprit.tn>
2022-07-25 14:49:06 +0200

    yassine update

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const { use } = require('express/lib/application');
+const app = express();
+const morgan= require('morgan');
+const mongoose= require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+app.use(cors());
+app.options('*', cors())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//Routes
+const usersRoutes = require('./routes/users');
+const { text } = require('body-parser');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const api = process.env.API_URL;
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(`${api}/users`, usersRoutes);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+
+mongoose.connect(process.env.CONNECTION_STRING,
+  {
+      useNewUrlParser : true,
+      useUnifiedTopology: true,
+      dbName: 'GestionEcole'
+
+
+  })
+.then(()=>{
+  console.log('connected ...')
+})
+.catch((err)=>{
+console.log(err);
+}) ;
+
+
+app.listen(3001, ()=>{
+  console.log(api);
+  console.log("app running in  http://localhost:3001");
+ 
+
+} )
